ec447b1eb766c57a532eec285d5a3583ba16dba2
h42koko34-ops <h42.koko34@gmail.com>
2026-09-22 22:01:44 +0300

    fix incomplete usestate

--- src/App.js
+++ src/App.js
@@ -201,10 +201,9 @@ export default function WheelsSkinsApp() {
   const handleNextTexture = () => {
     setActiveTextureIndex((prev) => (prev === leatherTextures.length - 1 ? 0 : prev + 1));
   };
-  const [touchStartX, setTouchStartX] = React.useState(0);
+const [touchStartX, setTouchStartX] = React.useState(0);
   const [dragOffset, setDragOffset] = React.useState(0);
   const [isSwiping, setIsSwiping] = React.useState(false);
-
   const handleTouchStart = (e) => {
     setTouchStartX(e.targetTouches[0].clientX);
     setIsSwiping(true);
